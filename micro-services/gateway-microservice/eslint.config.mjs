c224692730f169420fefdbc67a590b0f9347d45f
Paul OHL <paul.lv.ohl@gmail.com>
2024-10-11 12:38:30 +0200

    fix: Fixed eslint config

--- micro-services/gateway-microservice/eslint.config.mjs
+++ micro-services/gateway-microservice/eslint.config.mjs
@@ -6,4 +6,7 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
+  {
+    ignores: ['node_modules', 'dist', 'jest.config.js'],
+  }
 );
--- micro-services/gateway-microservice/jest.config.mjs
+++
@@ -1,5 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-export const testEnvironment = 'node';
-export const transform = {
-  '^.+.tsx?$': ['ts-jest', {}],
-};
