05a59d5bedaf8f69d6f53a1169731b27aee3d594
Paul OHL <paul.lv.ohl@gmail.com>
2024-10-11 11:52:36 +0200

    feat: Added a router and a basic route

--- micro-services/gateway-microservice/src/main.ts
+++ micro-services/gateway-microservice/src/main.ts
@@ -1,5 +1,19 @@
+import express, { Express } from 'express';
+import router from './routes/router';
+
 async function main() {
-  console.log("Hello, World!");
+  const appPort = process.env.GATEWAY_PORT;
+  if (!appPort) {
+    console.error('GATEWAY_PORT is not defined');
+    process.exit(1);
+  }
+
+  const app: Express = express();
+  app.use(express.json());
+  app.use('/', router);
+  app.listen(appPort, () => {
+    console.log(`Server is running on port ${appPort}`);
+  });
 }
 
 main();
